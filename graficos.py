e84317f544b9a343f79e1a0a8d45910f6cb3fb8c
Juan-Rolo-26 <140646508+Juan-Rolo-26@users.noreply.github.com>
2023-08-22 21:50:35 -0300

    graficos 2.1

--- graficos.py
+++ graficos.py
@@ -1,112 +1,69 @@
 import pygame
 import sys
 
-# Inicializar Pygame
 pygame.init()
 
-# Configuración de la ventana
-screen_width = 800
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("LPF - Manager")
+#configuración pantalla
+ancho = 800
+alto = 600
+pantalla = pygame.display.set_mode((ancho, alto))
+pygame.display.set_caption("Elegir Opción en Pygame")
+#paltea de colores
 
-# Cargar el fondo
-fondo = pygame.image.load("fondo.png")
-fondo = pygame.transform.scale(fondo, (screen_width, screen_height))
+Blanco = (255,255,255)
+Rojo = (255,0,0)
+Verde = (0,255,0)
+Azul = (0,0,255)
+Negro = (0,0,0)
 
-# Colores
-white = (255, 255, 255)
-black = (0, 0, 0)
+#fuente texto
+fuente = pygame.font.Font(None, 48)
+color_texto = (255, 255, 255)
 
-# Fuente
-font = pygame.font.Font(None, 36)
+#opciones
+opciones = [
+    "Información del juego",
+    "Elegir equipo"
+]
 
-# Estado actual del juego
-estado = "menu_principal"
+# Fondo de un juego de fútbol
+fondo_juego = pygame.image.load("fondo_futbol.jpg")
+fondo_juego = pygame.transform.scale(fondo_juego, (ancho, alto))
 
-# Equipos disponibles
-equipos20 = ["equipo1", "equipo2", "equipo3"]  # Agrega tus equipos disponibles aquí
+# Variable para almacenar el número ingresado
+numero_ingresado = ""
+opcion_seleccionada = None
 
-# Texto para la entrada de nombre de equipo
-input_text = ""
-input_rect = pygame.Rect(250, 300, 300, 40)
-active = False
-mensaje_info_juego = """
------Información General-----
-El juego consiste en un simulador de gestión de un equipo de fútbol
-al estilo FIFA. El jugador asume el papel de entrenador y tiene la opción
-de elegir uno de los equipos disponibles, como Boca Juniors, River Plate,
-Independiente, etc. Una vez seleccionado el equipo, el jugador puede
-acceder a un menú con varias opciones.
-¡Sé el gestor de tu equipo favorito! ;)
-Presiona ENTER para volver al menú principal.
-"""
-# Bucle principal
+#bucle juego
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if input_rect.collidepoint(event.pos):
-                active = not active
-            else:
-                active = False
+        if evento.type == pygame.KEYDOWN:
+            if evento.unicode.isnumeric():
+                numero_ingresado += evento.unicode
+            elif evento.key == pygame.K_RETURN:
+                if numero_ingresado:
+                    indice_opcion = int(numero_ingresado) - 1  
+                    if 0 <= indice_opcion < len(opciones):
+                        opcion_seleccionada = opciones[indice_opcion]
+                    numero_ingresado = ""
 
-        if event.type == pygame.KEYDOWN:
-            if active:
-                if event.key == pygame.K_RETURN:
-                    # Verificar si el nombre de equipo es válido
-                    equipo_elegido = input_text.lower()
-                    if equipo_elegido in equipos20:
-                        estado = "menu_equipo"
-                        active = False
-                        input_text = ""
-                elif event.key == pygame.K_BACKSPACE:
-                    input_text = input_text[:-1]
-                else:
-                    input_text += event.unicode
+    pantalla.blit(fondo_juego, (0, 0)) 
 
-    screen.blit(fondo, (0, 0))  # Dibujar el fondo
+    texto_y = 200
+    for i, opcion in enumerate(opciones, start=1):
+        texto_opcion = fuente.render(f"{i}. {opcion}", True, color_texto)
+        pantalla.blit(texto_opcion, (50, texto_y))
+        texto_y += 70
 
-    if estado == "menu_principal":
-        menu_text = font.render("-----LPF - Manager-----", True, black)
-        screen.blit(menu_text, (150, 100))
+    if opcion_seleccionada is not None:
+        texto_renderizado = fuente.render(f"Opción seleccionada: {opcion_seleccionada}", True, color_texto)
+        pantalla.blit(texto_renderizado, (50, alto - 100))
 
-        menu_text = font.render("1. Información del juego", True, black)
-        screen.blit(menu_text, (100, 200))
+    pygame.display.flip()
 
-        menu_text = font.render("2. Elegir equipo", True, black)
-        screen.blit(menu_text, (100, 250))
 
-        respuesta1 = pygame.key.get_pressed()
 
-        if respuesta1[pygame.K_1]:
-            estado = "info_juego"
-        elif respuesta1[pygame.K_2]:
-            estado = "elegir_equipo"
-    elif estado == "info_juego":
-        info_text = font.render(mensaje_info_juego, True, black)
-        screen.blit(info_text, (50, 100))
-
-        if pygame.key.get_pressed()[pygame.K_RETURN]:
-            estado = "menu_principal"
-    elif estado == "elegir_equipo":
-        equipos_text = font.render("Equipos disponibles:", True, black)
-        screen.blit(equipos_text, (100, 100))
-
-        for i, equipo in enumerate(equipos20, start=1):
-            equipo_text = font.render(f"{i}. {equipo.title()}", True, black)
-            screen.blit(equipo_text, (100, 150 + i * 50))
-
-        # Dibujar cuadro de entrada de nombre de equipo
-        txt_surface = font.render(input_text, True, black)
-        width = max(200, txt_surface.get_width() + 10)
-        input_rect.w = width
-        screen.blit(txt_surface, (input_rect.x + 5, input_rect.y + 5))
-        pygame.draw.rect(screen, black, input_rect, 2)
-
-        pygame.display.update()
-
-    pygame.display.update()
